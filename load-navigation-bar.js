7f6e019438989ff4b9762521e0bc1bdf08028dad
Rando <67262768+SomeNormalRando@users.noreply.github.com>
2026-03-10 11:14:37 +0800

    dropdown navbar for mobile

--- load-navigation-bar.js
+++ load-navigation-bar.js
@@ -1,51 +1,64 @@
-const links = [
-	[
-		"/",
+const ARROW_UP_ICON_PATH = "/assets/icons/keyboard_arrow_up_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg";
+const ARROW_DOWN_ICON_PATH = "/assets/icons/keyboard_arrow_down_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg";
+
+const NAVIGATION_LINKS = {
+	"/": [
 		"Home",
-		"./assets/icons/home_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg",
 	],
-	[
-		"/register",
+	"/register": [
 		"Register",
-		"./assets/icons/checkbook_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg",
 	],
-	[
-		"/resources",
+	"/resources": [
 		"Resources",
-		"./assets/icons/books_movies_and_music_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg",
 	],
-	[
-		"/faq",
+	"/faq": [
 		"FAQ",
-		"./assets/icons/contact_support_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg",
 	],
-	[
-		"/sponsors-and-partners",
+	"/sponsors-and-partners": [
 		"Sponsors & partners",
-		"./assets/icons/handshake_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg",
 	],
-];
+};
+
+const buttonDropdownNavbarToggle = document.createElement("button");
+const buttonText = document.createElement("span");
+const arrowIcon = document.createElement("img");
+arrowIcon.src = ARROW_DOWN_ICON_PATH;
+buttonText.textContent = "Home";
+
+buttonDropdownNavbarToggle.appendChild(buttonText);
+buttonDropdownNavbarToggle.appendChild(arrowIcon);
+
+buttonDropdownNavbarToggle.id = "button-dropdown-navbar-toggle";
 
 const ul = document.createElement("ul");
 
-for (const [href, name, iconPath] of links) {
-	const li = document.createElement("li");
-	const a = document.createElement("a");
-	a.href = href;
+buttonDropdownNavbarToggle.addEventListener("click", () => {
+	if (ul.style.display === "block") {
+		arrowIcon.src = ARROW_DOWN_ICON_PATH;
+		ul.style.display = "none";
+		return;
+	}
+	ul.style.display = "block";
+	arrowIcon.src = ARROW_UP_ICON_PATH;
+});
 
-	const img = document.createElement("img");
-	img.classList.add("nav-link-icon");
-	img.src = iconPath;
+
+for (const [path, [name]] of Object.entries(NAVIGATION_LINKS)) {
+	const li = document.createElement("li");
+	if (window.location.pathname === path) li.classList.add("current-page");
+
+	const a = document.createElement("a");
+	a.href = path;
 
 	const span = document.createElement("span");
 	span.classList.add("nav-link-anchor-text");
 	span.textContent = name;
 
-	a.appendChild(img);
 	a.appendChild(span);
 	li.appendChild(a);
 	ul.appendChild(li);
 }
 
 const nav = document.querySelector("nav");
+nav.appendChild(buttonDropdownNavbarToggle);
 nav.appendChild(ul);
