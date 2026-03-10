434349334a600711fc2787e81937de6999954abb
Rando <67262768+SomeNormalRando@users.noreply.github.com>
2026-03-10 16:41:46 +0800

    add footer bar - scales automatically on mobile too but padding/margins might need some adjustment

---
+++ scripts/load-footer-bar.js
@@ -0,0 +1,40 @@
+const footer = document.querySelector("footer");
+footer.innerHTML = `
+<div id="footer-intro-container">
+	<span class="organisation-logo footer-heading">
+		<span class="text-logo-first-letter">T</span><span class="text-logo-after-first-letter">iger Tank</span>
+		<span class="text-logo-malaysia">Malaysia</span>
+	</span>
+	<span class="footer-motto">Introducing entrepreneurship to Malaysian students.</span>
+</div>
+<div>
+	<address>
+		<dl>
+			<dt class="footer-heading">Contact us</dt>
+			<dd>
+				<ul>
+					<li><a href="mailto:tigertankmalaysia@gmail.com">tigertankmalaysia@gmail.com</a></li>
+					<li><a href="tel:+60166658769">+60 16-665 8769</a> (Marcus Ong, director)</li>
+				</ul>
+			</dd>
+		</dl>
+	</address>
+</div>
+<div>
+	<address>
+		<dl>
+			<dt class="footer-heading">Connect with us</dt>
+			<dd>
+				<ul id="social-media-list">
+					<li><a href="https://instagram.com/tigertankmy" data-icon="instagram"></a></li>
+					<li><a href="https://tiktok.com/@tigertankmy" data-icon="tiktok"></a></li>
+					<li><a href="https://linkedin.com/company/tiger-tank-malaysia" data-icon="linkedin"></a></li>
+				</ul>
+			</dd>
+		</dl>
+	</address>
+</div>
+<div>
+	<a href="" class="footer-heading">Privacy policy</a>
+</div>
+`;
